ecddd59bbedd74c8062a3f27663692a5651caa1f
BMCII <brian.m.cobbii@fullsail.edu>
2016-04-22 23:17:57 -0400

    got edit form up

--- band_together/app/Http/Controllers/PlayerController.php
+++ band_together/app/Http/Controllers/PlayerController.php
@@ -4,24 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Player;
+
 use App\Http\Requests;
 
 class PlayerController extends Controller
 {
-    public function store(Request $request, Player $player){
 
-        $player = new Player;
-
-        $player->stage_name = $request->stage_name;
-
-        $player->age = $request->age;
-
-        $player->instrument = $request->instrument;
-
-        $player->style = $request->style;
-
-        $player->players()->save($player);
-
-        return back();
+    public function profile_form(){
+        return view('players.profile_form');
     }
+
+//    public function store(Request $request, Player $player){
+//
+//        $player = new Player;
+//
+//        $player->stage_name = $request->stage_name;
+//
+//        $player->age = $request->age;
+//
+//        $player->instrument = $request->instrument;
+//
+//        $player->style = $request->style;
+//
+//        $player->players()->save($player);
+//
+//        return back();
+//    }
 }
--- band_together/resources/views/home.blade.php
+++ band_together/resources/views/home.blade.php
@@ -3,14 +3,36 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
                 <div class="panel-heading">Stage Name</div>
 
                 <div class="panel-body">
                     <span>Name: {{ Auth::user()->name }}</span> <br />
                     <span>Email: {{ Auth::user()->email }}</span>
-                    <a class="btn-primary">Edit Account</a>
+
+                    <form method="post" >
+                        <div class="form-group">
+                            <label for="player">Stage Name:</label>
+                            <input type="text" name="stage_name" class="form-control" id="player">
+                        </div>
+                        <div class="form-group">
+                            <label for="age">Age:</label>
+                            <input type="number" class="form-control" id="age">
+                        </div>
+                        <div class="form-group">
+                            <label for="instrument">Instrument:</label>
+                            <input type="text" class="form-control" id="instrument">
+                        </div>
+                        <div class="form-group">
+                            <label for="style">Style:</label>
+                            <input type="text" class="form-control" id="style">
+                        </div>
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <button type="submit" href="{{ url('/players/profile_form') }}" class="btn-primary  col-md-offset-10">Create Player</button>
+                    </form>
+
+
                 </div>
             </div>
         </div>
